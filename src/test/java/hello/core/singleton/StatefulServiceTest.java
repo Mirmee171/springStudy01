27785216bd6bf0aa95a344e56f2a4111adf0f982
Mirmee171 <96275034+Mirmee171@users.noreply.github.com>
2024-01-09 00:13:10 +0900

    Initial commit

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        int orderAPrice = statefulService1.order("orderA", 10000);
+        int orderBPrice = statefulService2.order("orderB", 20000);
+
+        System.out.println("orderAPrice = " + orderAPrice);
+
+        Assertions.assertThat(orderAPrice).isEqualTo(10000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
